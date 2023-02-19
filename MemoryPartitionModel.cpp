f4a0048f4f68a981046519d78c45513178df86fe
SHUlinlulu <linlulu@shu.edu.cn>
2023-02-19 17:04:25 +0800

    增加了测试栈区和堆区的代码，主要考量的是栈区注意事项，即不能返回局部变量的地址

--- MemoryPartitionModel.cpp
+++ MemoryPartitionModel.cpp
@@ -11,7 +11,10 @@ using namespace std;
 * 5.“栈区”存放那些程序执行过程中的局部变量，局部常量，如：函数返回时Compiler临时创建的
 * temp变量，函数调用时，参数亦或递归函数执行时的临时参数等皆在这个区内，由Compiler管理
 * 6.C++中的“堆区”，由程序员管理和回收，主要使用关键字"new"和“delete”
-* 注意：“堆区管理”，程序员必须铭记要在申请的堆空间变量使用完后，及时释放（面试常考）
+* 注意：
+* 1.“堆区管理”，程序员必须铭记要在申请的堆空间变量使用完后，及时释放（面试常考）;
+* 2."栈区管理"注意：“不要返回局部变量的地址，比如：函数调用返回值是一个地址（函数内部变量）”
+* 就会造成错误，当然，compiler会做一次暂时的保留，下次再用就会出错
 */
 //定义一个全局变量
 int g_v = 10;
@@ -21,6 +24,8 @@ const double g_c = 3.1415;
 static int g_s = 50;
 void static demo();
 void Heap();
+void test2();
+int* localAddreRet();
 int main(int argc, char* argv[]);//没这一行，complier找不到main,会报错（因main声明在后）
 //写一个测试函数
 void test() {
@@ -46,7 +51,11 @@ void test() {
 void static demo(){}
 //main函数
 int main(int argc, char *argv[]) {
-	test();
+	/*test();*/
+	test2();
+	//int* temp;
+	//temp = localAddreRet();//直接报错
+	//cout << "*temp=" << *temp << endl;
 	system("pause");
 	return 0;
 }
@@ -61,4 +70,32 @@ void Heap() {
 	p = NULL;//正确的规范，防止产生“野指针”----指向未知区域（无权限）
 	delete[]p2;//一定要记得使用完释放空间，毕竟这是在堆里自己开辟的
 	p2 = NULL;
+}
+
+//定义一个返回局部变量地址的函数,此时局部变量内存开辟在栈区
+int* localAddreRet() {
+	int a = 10;
+	cout << "a=" <<*&a<< endl;//Attention
+	return &a;
+}
+//定义一个返回局部变量地址的函数,此时局部变量内存开辟在堆区
+int* localAddreRet2() {
+	int* p = new int(10);
+	cout << "*p=" << *p << endl;
+	return p;
+}
+//写一个测试函数
+void test2() {
+	int* temp;
+	temp = localAddreRet();//直接报错
+	cout << "*temp=" << *temp << endl;//不行了，现在VS的C++ Compiler好像不对返回的局部变量地址做一次保留了
+	cout << "*temp=" << *temp << endl;//出错，无权访问，程序调用结束后局部变量在栈中的空间就被释放掉了
+
+	temp = localAddreRet2();//直接报错
+	cout << "*temp=" << *temp << endl;//正确，因：程序员未将开辟的堆区空间释放掉，在程序整体没有完成前，该内存空间地址一直被占用
+	cout << "*temp=" << *temp << endl;
+	if (!temp) {
+		delete temp;
+		temp = NULL;//置空，免（“野指针”）
+	}
 }
